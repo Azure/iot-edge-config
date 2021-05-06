0703a3db976e4bf3402684915c92b3ae8d7b0e3c
Cindy Deng <cindydeng@microsoft.com>
2021-05-06 11:53:13 -0700

    Added helper files as release assets and wget added to main script

--- src/azure-iot-edge-installer.sh
+++ src/azure-iot-edge-installer.sh
@@ -1,5 +1,19 @@
-#script 
+# script 
 echo "Running azure-iot-edge-installer.sh"
+
+# if helper scripts dont exist, fetch via wget 
+if [ -d "iot-edge-installer" ] 
+then
+    echo "Directory iot-edge-installer exists." 
+else
+    mkdir iot-edge-installer
+    cd iot-edge-installer
+    wget https://github.com/Azure/iot-edge-config/releases/latest/download/validate-tier1-os.sh
+    wget https://github.com/Azure/iot-edge-config/releases/latest/download/install-container-management.sh
+    wget https://github.com/Azure/iot-edge-config/releases/latest/download/install-edge-runtime.sh
+    wget https://github.com/Azure/iot-edge-config/releases/latest/download/validate-post-install.sh
+    echo "Downloaded helper files to temporary directory /iot-edge-installer"
+fi
 
 # add permission to run
 chmod +x validate-tier1-os.sh
@@ -12,3 +26,11 @@ chmod +x validate-post-install.sh
 ./install-container-management.sh
 ./install-edge-runtime.sh
 ./validate-post-install.sh
+cd ..
+
+# cleanup
+if [ -d "iot-edge-installer" ] 
+then
+    rm -rf iot-edge-installer
+    echo "Removed temporary directory files for iot-edge-installer" 
+fi
