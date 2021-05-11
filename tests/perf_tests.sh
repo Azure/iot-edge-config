366ec7363a47276068b76a4632a49b26852f5f17
Haitham Shami <hshami@microsoft.com>
2021-05-10 17:20:11 -0700

    try 2of infinity

--- tests/perf_tests.sh
+++ tests/perf_tests.sh
@@ -15,4 +15,5 @@
 subscription=$(az account show | awk '/"id/ { print substr($2,2,36) }')
 
 # for each test ...
+echo ./track_duration.sh -c $1 -t ./e2e-tests/test-devicestate.sh $subscription
 ./track_duration.sh -c $1 -t ./e2e-tests/test-devicestate.sh $subscription
