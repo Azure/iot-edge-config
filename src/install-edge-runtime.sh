835c53d5794d437f2cb8f65ca514ba2b5a7c94ac
Cindy Deng <cindydeng@microsoft.com>
2021-05-12 10:14:13 -0700

    string replacement init

--- src/install-edge-runtime.sh
+++ src/install-edge-runtime.sh
@@ -1,2 +1,21 @@
 #script to install edge runtime 1.2
 echo "Running install-edge-runtime.sh"
+
+# sudo apt-get update
+
+# install Azure IoT Edge Runtime 1.2
+# sudo apt-get install aziot-edge -y
+
+# create .toml from template 
+# sudo cp /etc/aziot/config.toml.edge.template /etc/aziot/config.toml
+
+sed -i '/## Manual provisioning with connection string/,/## Manual provisioning with symmetric key/c\
+## Manual provisioning with connection string\
+[provisioning]\
+source = $1\
+connection_string = $2\
+
+## Manual provisioning with symmetric key\
+'  sample.toml
+
+# sudo iotedge config apply
