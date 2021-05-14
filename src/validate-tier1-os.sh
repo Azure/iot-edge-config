2b8cd16267ab975ada519013e601911c67ae17ff
Haitham Shami <hshami@microsoft.com>
2021-05-13 17:58:02 -0700

    fixes based on side discussions

--- src/validate-tier1-os.sh
+++ src/validate-tier1-os.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
 ###################################### 
 # validate-tier1-os.sh
 # 
@@ -7,35 +10,31 @@
 # https://docs.microsoft.com/en-us/azure/iot-edge/support?view=iotedge-2020-11#tier-1
 # ARGUMENTS:
 #    Current OS ID
-#   Current OS VERSION ID
+#    Current OS VERSION ID
 # OUTPUTS:
 #    Write output to stdout
 # RETURN:
 #    0 if OS is tier 1, 1 otherwise
 ######################################
 
-source utils.sh
-
-function is_os_tier1() {    
-
+function is_os_tier1() {
     local os_id=$1
     local os_version_id=$2
     log_info "OS ID: '%s'; OS Version ID: '%s'" $os_id $os_version_id
 
     case $os_id in
-
         ubuntu)
-            echo "OS is Ubuntu"
             if [ "$os_version_id" == "18.04" ];
             then
-                echo "Version is 18.04"
                 return 0
-            fi;
+            fi
             ;;
 
         raspbian)
-            echo "OS is Raspbian"
-            return 0
+            if [ $os_version_id == "stretch" ];
+            then
+                return 0
+            fi
             ;;
 
         *)
@@ -46,23 +45,4 @@ function is_os_tier1() {
     return 1
 }
 
-function get_platform() {
-    local os_id=$1
-    local os_version_id=$2
-    local os_platform=""
-    
-    case $os_id in
-        ubuntu)
-            if [ $os_version_id == "18.04" ];  
-            then
-                os_platform="ubuntu/18.04"
-            fi
-            ;;
-
-        raspbian)
-            os_platform="debian/stretch"
-            ;;
-    esac
-    
-    echo "$os_platform"
-}
+export -f is_os_tier1
