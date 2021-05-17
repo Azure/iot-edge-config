f56d95b67c18955ba0fe7578c0394329df35ebf6
Haitham Shami <hshami@microsoft.com>
2021-05-17 10:47:19 -0700

    update command line argument parser (#19)
    * update command line argument parser
    
    * update with PR feedback

--- tests/unit-tests/test-cmd-parser.sh
+++ tests/unit-tests/test-cmd-parser.sh
@@ -3,13 +3,22 @@
 # import utils.sh
 source ../../src/utils.sh
 
+# add flag:variable_name dictionary entries
+add_option_args "VERBOSE_LOGGING" -v --verbose
+add_option_args "SCOPE_ID" -s --scope-id
+add_option_args "REGISTRATION_ID" -r --registration-id
+add_option_args "SYMMETRIC_KEY" -k --symmetric-key
+add_option_args "DEVICE_PROVISIONING" -dp
+add_option_args "AZURE_CLOUD_ID" -ap
+
 # unit test to test all illegal flags, output should be empty
 test_illegal_flags() {
     # parse sample input to parser
     declare -A parsed_cmds="$(cmd_parser -illegal val -t anotherillegalval -z yetanotherillegalval)"
 
     # compare output, should be empty
-    if [ "${parsed_cmds[*]}" != "" ]; then
+    if [ "${parsed_cmds[*]}" != "" ];
+    then
         echo "Failed to pass unit test 'test_illegal_flags' in test-cmd-parser.sh. Non-empty result: ${parsed_cmds[*]}"
     fi
 }
@@ -19,7 +28,8 @@ test_all_legal_flags() {
     declare -A parsed_cmds="$(cmd_parser -dp 1 -ap 5 -s 2 -r 4 -k 3 -v 6)"
 
     # compare output, should be 123456
-    if [ "${parsed_cmds[*]}" != "1 2 3 4 5 6" ]; then
+    if [[ "${parsed_cmds['DEVICE_PROVISIONING']}" != "1" || "${parsed_cmds['AZURE_CLOUD_ID']}" != "5" || "${parsed_cmds['SCOPE_ID']}" != "2" || "${parsed_cmds['REGISTRATION_ID']}" != "4" || "${parsed_cmds['SYMMETRIC_KEY']}" != "3" || "${parsed_cmds['VERBOSE_LOGGING']}" != "6" ]];
+    then
         echo "Failed to pass unit test 'test_all_legal_flags' in test-cmd-parser.sh. Non-empty result: ${parsed_cmds[*]}"
     fi
 }
@@ -29,8 +39,9 @@ test_extra_arguments() {
     declare -A parsed_cmds="$(cmd_parser illegalinput 0 -ap 4 -s 1 -r 3 more -extra -k 2)"
 
     # compare output, should be 1 2 3 4
-    if [ "${parsed_cmds[*]}" != "1 2 3 4" ]; then
-        echo "Failed to pass unit test 'test_extra_arguments' in test-cmd-parser.sh. Non-empty result: ${parsed_cmds[*]}"
+    if [ "${parsed_cmds[*]}" != "" ];
+    then
+        echo "Failed to pass unit test 'test_extra_arguments' in test-cmd-parser.sh"
     fi
 }
 
