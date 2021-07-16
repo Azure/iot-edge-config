41bb8c7cf720050545a570be179a1cb2fa160edb
Haitham Shami <hshami@microsoft.com>
2021-07-16 10:00:19 -0700

    generate simple config.toml (#39)
    * generate simple config.toml
    
    * Update with PR Comments

--- src/install-edge-runtime.sh
+++ src/install-edge-runtime.sh
@@ -47,45 +47,33 @@ function install_edge_runtime() {
     fi
     log_info "Installed edge runtime..."
 
-    # create .toml from template
-    log_info "Create instance configuration .toml from template."
-    cp /etc/aziot/config.toml.edge.template /etc/aziot/config.toml 2>>$STDERR_REDIRECT 1>>$STDOUT_REDIRECT
-    exit_code=$?
-    if [[ $exit_code != 0 ]];
-    then
-        log_info "'cp /etc/aziot/config.toml.edge.template /etc/aziot/config.toml' returned %d" $exit_code
-        exit ${EXIT_CODES[11]}
-    fi
+    # create config.toml
+    log_info "Create instance configuration 'config.toml'."
 
     local SCOPE_ID=$1
     local REGISTRATION_ID=$2
     local SYMMETRIC_KEY=$3
 
     log_info "Set DPS provisioning parameters."
-    sed -i '/## DPS provisioning with symmetric key/,/## DPS provisioning with X.509 certificate/c\
-## DPS provisioning with symmetric key\
-[provisioning]\
-source = "dps"\
-global_endpoint = "https://global.azure-devices-provisioning.net"\
-id_scope = \"'$SCOPE_ID'\"\
-\
-[provisioning.attestation]\
-method = "symmetric_key"\
-registration_id = \"'$REGISTRATION_ID'\"\
-\
-symmetric_key = { value = \"'$SYMMETRIC_KEY'\" }                                                                         # inline key (base64), or...\
-# symmetric_key = { uri = "file:///var/secrets/device-id.key" }                                                          # file URI, or...\
-# symmetric_key = { uri = "pkcs11:slot-id=0;object=device%20id?pin-value=1234" }                                         # PKCS#11 URI\
-\
-## DPS provisioning with X.509 certificate\
-    '  /etc/aziot/config.toml 2>>$STDERR_REDIRECT 1>>$STDOUT_REDIRECT
 
-    exit_code=$?
-    if [[ $exit_code != 0 ]];
-    then
-        log_info "'sed ....'" $exit_code
-        exit ${EXIT_CODES[12]}
-    fi
+    local FILE_NAME="/etc/aziot/config.toml"
+
+    # create a config.toml - will replace existing
+    echo 'hostname = "'`hostname`'"' > $FILE_NAME
+    echo '' >> $FILE_NAME
+    echo '## DPS provisioning with symmetric key' >> $FILE_NAME
+    echo '[provisioning]' >> $FILE_NAME
+    echo 'source = "dps"' >> $FILE_NAME
+    echo '' >> $FILE_NAME
+    echo 'global_endpoint = "https://global.azure-devices-provisioning.net"' >> $FILE_NAME
+    echo 'id_scope = "'$SCOPE_ID'"' >> $FILE_NAME
+    echo '' >> $FILE_NAME
+    echo '[provisioning.attestation]' >> $FILE_NAME
+    echo 'method = "symmetric_key"' >> $FILE_NAME
+    echo 'registration_id = "'$REGISTRATION_ID'"' >> $FILE_NAME
+    echo '' >> $FILE_NAME
+    echo 'symmetric_key = { value = "'$SYMMETRIC_KEY'" }' >> $FILE_NAME
+    echo '' >> $FILE_NAME
 
     log_info "Apply settings - this will restart the edge"
     iotedge config apply 2>>$STDERR_REDIRECT 1>>$STDOUT_REDIRECT
