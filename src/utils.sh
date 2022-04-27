450128c7693a772c70b5f86ad9bba55a53653aa9
Haitham Shami <hshami@microsoft.com>
2022-04-27 15:35:09 -0700

    y

--- src/utils.sh
+++ src/utils.sh
@@ -490,18 +490,18 @@ function handle_exit() {
         log_info "Removed temporary directory files for iot-edge-installer."
     fi
 
-    if [[ "$LOCAL_E2E" == "1" ]];
-    then
-        if [[ $e_code != 0 ]];
-        then
-            echo errors-file -----------------------------
-            cat $STDERR_REDIRECT
-            echo errors-file -----------------------------
-        fi
-        echo stdout-file -----------------------------
-        cat $STDOUT_REDIRECT
-        echo stdout-file -----------------------------
-    fi
+#    if [[ "$LOCAL_E2E" == "1" ]];
+#    then
+#        if [[ $e_code != 0 ]];
+#        then
+#            echo errors-file -----------------------------
+#            cat $STDERR_REDIRECT
+#            echo errors-file -----------------------------
+#        fi
+#        echo stdout-file -----------------------------
+#        cat $STDOUT_REDIRECT
+#        echo stdout-file -----------------------------
+#    fi
 
     announce_my_log_file "All logs were appended to" $OUTPUT_FILE
 }
